4acb4fd4c37adc4633de7fc0d12c5f5aff873254
dadajam4 <dadajam4@gmail.com>
2022-07-21 17:35:51 +0900

    chore: work

--- site/src/pages/getting-started/index.tsx
+++ site/src/pages/getting-started/index.tsx
@@ -1,12 +1,47 @@
 import { defineComponent } from 'vue';
-import { VPage } from '@fastkit/vui';
+import { VPage, VSheetStack } from '@fastkit/vui';
 import { VDocsLayout } from '~/components';
+import { range } from '@fastkit/helpers';
 
 export default defineComponent({
   render() {
     return (
       <VDocsLayout home="/getting-started" title="HOME">
         <VPage />
+        <button type="button" onClick={(ev) => this.$vui.stack.sheet('xxxx')}>
+          click!!
+        </button>
+        <VSheetStack
+          backdrop
+          v-slots={{
+            activator: ({ attrs }) => (
+              <button {...attrs} type="button">
+                xxxx
+              </button>
+            ),
+          }}>
+          <div>
+            <VSheetStack
+              backdrop
+              v-slots={{
+                activator: ({ attrs }) => (
+                  <button {...attrs} type="button">
+                    xxxx
+                  </button>
+                ),
+              }}>
+              <div>
+                {range(100, 1).map((i) => (
+                  <div key={i} class="p-4">{`テキスト${i}が入ります。`}</div>
+                ))}
+              </div>
+            </VSheetStack>
+
+            {range(100, 1).map((i) => (
+              <div key={i} class="p-4">{`テキスト${i}が入ります。`}</div>
+            ))}
+          </div>
+        </VSheetStack>
       </VDocsLayout>
     );
   },
