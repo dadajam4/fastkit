27378703748e22d01fcb516e489e820d7d68ca31
dadajam4 <dadajam4@gmail.com>
2023-05-02 09:59:29 +0900

    feat(helpers): add strip indent

--- packages/helpers/src/string.ts
+++ packages/helpers/src/string.ts
@@ -51,3 +51,51 @@ export function capitalize<T extends string>(str: T): Capitalize<T> {
 export function uncapitalize<T extends string>(str: T): Uncapitalize<T> {
   return (str.charAt(0).toLowerCase() + str.slice(1)) as Uncapitalize<T>;
 }
+
+const INDENT_MATCH_RE = /^[ \t]*(?=\S)/gm;
+
+/**
+ * Ignore blank lines and get the number of indent characters in the line with the lowest number of leading spaces
+ *
+ * @param str - String to get indentation
+ * @returns Minimum indent character count
+ */
+export function minIndent(str: string): number {
+  const match = str.match(INDENT_MATCH_RE);
+
+  if (!match) {
+    return 0;
+  }
+
+  return match.reduce((r, a) => Math.min(r, a.length), Infinity);
+}
+
+const STRIP_INDENT_UNNECESSARY_LINE_MATCH_RE = /(^\n+|\n\s+$)/g;
+
+/**
+ * Obtain a string with the minimum number of indent characters removed
+ *
+ * @remarks This method trims unnecessary lines (newlines, whitespace only) before and after. This process can be disabled with `retainUnnecessaryLines`
+ *
+ * @param str - String to remove indentation
+ * @param retainUnnecessaryLines - Retain unnecessary lines
+ * @returns String with normalized indentation
+ */
+export function stripIndent(
+  str: string,
+  retainUnnecessaryLines?: boolean,
+): string {
+  if (!retainUnnecessaryLines) {
+    str = str.replace(STRIP_INDENT_UNNECESSARY_LINE_MATCH_RE, '');
+  }
+
+  const indent = minIndent(str);
+
+  if (indent === 0) {
+    return str;
+  }
+
+  const regex = new RegExp(`^[ \\t]{${indent}}`, 'gm');
+
+  return str.replace(regex, '');
+}
