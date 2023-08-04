f44fa2be6bef32a0b83a4f7aa2cdf8e6546b0ac4
dadajam4 <dadajam4@gmail.com>
2023-08-04 15:30:40 +0900

    refactor: add jsdocs

--- packages/catcher/src/resolvers/fetch.ts
+++ packages/catcher/src/resolvers/fetch.ts
@@ -1,5 +1,12 @@
+/**
+ * @file Resolver to resolve Fetch API Exceptions
+ */
+
 import { createCatcherResolver } from '../schemes';
 
+/**
+ * JSON serializable Fetch Response
+ */
 export interface SerializableFetchResponse {
   headers: Headers;
   ok: boolean;
@@ -12,45 +19,119 @@ export interface SerializableFetchResponse {
   text: string;
 }
 
-export interface FetchError {
-  message: string;
+/**
+ * Serializable Fetch Error
+ */
+export interface SerializableFetchError {
+  name?: string;
+  message?: string;
+  stack?: string;
+  /** {@link SerializableFetchResponse JSON serializable Fetch Response } */
   response: SerializableFetchResponse;
 }
 
+/**
+ * Override Fetch Error
+ */
 export interface FetchErrorOverrides {
-  fetchError: SerializableFetchResponse;
-  // name: string;
-  // message: string;
-  // fetchResponse: SerializableFetchResponse;
+  /**
+   * {@link SerializableFetchResponse Fetch Error information }
+   *
+   * Only set when an axios exception is detected
+   */
+  fetchError: SerializableFetchError;
 }
 
-export const fetchResponseResolver = createCatcherResolver(
-  function fetchResponseResolver(source, ctx): FetchErrorOverrides | undefined {
-    if (typeof Response === 'undefined' || !(source instanceof Response))
-      return;
+function isResponse(source: unknown): source is Response {
+  if (!source || typeof Response === 'undefined') return false;
+  return source instanceof Response;
+}
+
+/**
+ * Extracted Fetch Error
+ */
+export interface ExtractedFetchError {
+  name?: string;
+  message?: string;
+  stack?: string;
+  /** Fetch Response */
+  response: Response;
+}
+
+/**
+ * Extract function for fetch errors
+ */
+export type ExtractFetchError = (
+  source: unknown,
+) => ExtractedFetchError | undefined;
+
+const DEFAULT_EXTRACT_FETCH_ERROR: ExtractFetchError = (source) => {
+  if (isResponse(source)) {
+    return {
+      response: source,
+    };
+  }
+  if (source instanceof Error) {
+    const maybeResponse = (source as any).response || (source as any).res;
+    const response = isResponse(maybeResponse) ? maybeResponse : undefined;
+    if (!response) return;
+
+    return {
+      name: source.name,
+      message: source.message,
+      stack: source.stack,
+      response,
+    };
+  }
+};
+
+/**
+ * Resolver to resolve fetch exceptions
+ *
+ * * Since the Fetch API does not throw communication exceptions, this resolver expects the application to throw an exception in response to the fetch result
+ * * Since the structure of exceptions to throw varies from application to application, this method can be used to set up a custom error extraction function
+ * * If the error extraction function is unspecified, the exception to be thrown is expected to be a Response object, or to have a response object named `response` or `res` in the error instance.
+ */
+export const fetchResponseResolver = (
+  extract: ExtractFetchError = DEFAULT_EXTRACT_FETCH_ERROR,
+) =>
+  createCatcherResolver(function fetchResponseResolver(
+    source,
+    ctx,
+  ): FetchErrorOverrides | undefined {
+    const extracted = extract(source);
+    if (!extracted) return;
+
+    const { name, message, stack, response } = extracted;
+
     const fetchResponse: SerializableFetchResponse = {
-      headers: source.headers,
-      ok: source.ok,
-      redirected: source.redirected,
-      status: source.status,
-      statusText: source.statusText,
-      type: source.type,
-      url: source.url,
+      headers: response.headers,
+      ok: response.ok,
+      redirected: response.redirected,
+      status: response.status,
+      statusText: response.statusText,
+      type: response.type,
+      url: response.url,
       json: null,
       text: '',
     };
 
-    source.json().then((json) => {
+    response.json().then((json) => {
       fetchResponse.json = json;
     });
-    source.text().then((text) => {
+
+    response.text().then((text) => {
       fetchResponse.text = text;
     });
 
     ctx.resolve();
 
     return {
-      fetchError: fetchResponse,
+      fetchError: {
+        name,
+        message,
+        stack,
+        response: fetchResponse,
+      },
     };
-  },
-);
+  });
--- packages/catcher/src/schemes.ts
+++ packages/catcher/src/schemes.ts
@@ -1,7 +1,10 @@
 import type { nativeErrorResolver } from './resolvers/native';
 import type { UnionToIntersection } from '@fastkit/helpers';
 
-type BultinErrorResolvers = [typeof nativeErrorResolver];
+/**
+ * List of built-in resolvers
+ */
+type BuiltinErrorResolvers = [typeof nativeErrorResolver];
 
 export interface AnyData {
   [key: string]: any;
@@ -15,7 +18,6 @@ interface ErrorImplements {
 
 type ExcludeNullableReturnType<T> = T extends (...args: any[]) => any
   ? (
-      // ...args: Parameters<T>
       args: Parameters<T>[0],
     ) => Partial<Exclude<ReturnType<T>, void | undefined | null>>
   : (...args: any[]) => {};
@@ -24,13 +26,21 @@ export type ResolverContext = {
   resolve: () => void;
 };
 
+/** Exception Resolver */
 export type AnyResolver = (
   exceptionInfo: unknown,
   ctx: ResolverContext,
 ) => AnyData | void | undefined | null;
 
+/** List of Exception Resolver */
 export type AnyResolvers = AnyResolver[];
 
+/**
+ * Creating an Exception Resolver
+ *
+ * @param resolver -  Exception Resolver
+ * @returns Exception Resolver
+ */
 export function createCatcherResolver<Resolver extends AnyResolver>(
   resolver: Resolver,
 ) {
@@ -47,57 +57,117 @@ type ComputedResolvedTypes<Resolvers extends AnyResolvers = AnyResolvers> =
   >;
 
 export type ResolvedCatcherData<Resolvers extends AnyResolvers = AnyResolvers> =
-  ComputedResolvedTypes<[...BultinErrorResolvers, ...Resolvers]>;
+  ComputedResolvedTypes<[...BuiltinErrorResolvers, ...Resolvers]>;
 
+/**
+ * Exception Normalizer
+ *
+ * A method to finally normalize the set of values resolved by the resolver
+ */
 export type AnyNormalizer<Resolvers extends AnyResolvers = AnyResolvers> = (
   resolvedData: ResolvedCatcherData<Resolvers>,
 ) => (exceptionInfo: any) => AnyData | void | undefined | null;
 
-export function createCatcherNomalizer<
+/**
+ * Creating an Exception Normalizer
+ *
+ * @param normalizer - Exception Normalizer
+ * @param _resolvers - List of Exception Resolver
+ * @returns Exception Normalizer
+ */
+export function createCatcherNormalizer<
   Normalizer extends AnyNormalizer<Resolvers>,
   Resolvers extends AnyResolvers = AnyResolvers,
->(nomalizer: Normalizer, _resolvers?: Resolvers): Normalizer {
-  return nomalizer;
+>(normalizer: Normalizer, _resolvers?: Resolvers): Normalizer {
+  return normalizer;
 }
 
+/**
+ * Catcher constructor generation options
+ */
 export interface CatcherBuilderOptions<
   Resolvers extends AnyResolvers,
   Normalizer extends AnyNormalizer<Resolvers>,
 > {
+  /**
+   * Default name if the name cannot be resolved from the supplemented exception
+   *
+   * @default CatcherError
+   */
   defaultName?: string;
+  /**
+   * {@link Resolvers List of Exception Resolver}
+   */
   resolvers?: Resolvers;
+  /**
+   * Exception Normalizer
+   *
+   * A method to finally normalize the set of values resolved by the resolver
+   */
   normalizer: Normalizer;
 }
 
 export type CatcherData<T = AnyData> = T & {
+  /**
+   * Catcher symbol
+   * @internal
+   */
   $__catcher: true;
 };
 
+/**
+ * Caught exception instances
+ */
 export interface Catcher<
   Resolvers extends AnyResolvers = AnyResolvers,
   T = AnyData,
 > extends Error {
+  /** It is a catcher instance */
   readonly isCatcher: true;
+  /** Error information fully processed by custom resolvers and normalizers */
   readonly data: CatcherData<T>;
+  /** Data extracted by custom resolvers */
   readonly resolvedData: ResolvedCatcherData<Resolvers>;
+  /**
+   * Catcher instance generated from original exception source before override
+   *
+   * * Only set if override is specified at instance creation
+   */
   readonly source?: Catcher<Resolvers, T>;
+  /**
+   * List of extension sources for own instance
+   *
+   * * The younger the index, the closer the source is to you.
+   */
   readonly histories: Catcher<Resolvers, T>[];
+  /**
+   * 全ての歴史を辿ったメッセージのリスト
+   *
+   * * If multiple identical messages are detected, they are filtered to avoid duplicates
+   */
   readonly messages: string[];
-  // source: any;
+  /**
+   * Obtain as a JSON object
+   */
   toJSON(): ErrorImplements & CatcherData<T> & { messages: string[] };
+  /**
+   * Obtain as JSON string
+   * @param indent - number of indentations
+   *
+   * @default 2
+   */
   toJSONString(indent?: number | boolean): string;
 }
 
+/**
+ * Exception catcher constructor
+ *
+ * * Instance properties are extended by type arguments at creation
+ */
 export interface CatcherConstructor<
   Resolvers extends AnyResolvers = AnyResolvers,
   Normalizer extends AnyNormalizer = AnyNormalizer,
 > {
-  // new (
-  //   exceptionInfo: Parameters<ReturnType<Normalizer>>[0],
-  //   overrides?: Partial<ReturnType<ReturnType<Normalizer>> & ErrorImplements>,
-  // ): Catcher<Resolvers, ReturnType<ReturnType<Normalizer>>> &
-  //   ReturnType<ReturnType<Normalizer>>;
-
   /**
    * Create an error instance based on the error information.
    */
@@ -124,14 +194,4 @@ export interface CatcherConstructor<
     overrides?: Partial<ReturnType<ReturnType<Normalizer>> & ErrorImplements>,
   ): Catcher<Resolvers, ReturnType<ReturnType<Normalizer>>> &
     ReturnType<ReturnType<Normalizer>>;
-  // new (
-  //   exceptionInfo: Parameters<ReturnType<Normalizer>>[0],
-  //   overrides?: Partial<ReturnType<ReturnType<Normalizer>> & ErrorImplements>,
-  // ): Catcher<Resolvers, ReturnType<ReturnType<Normalizer>>> &
-  //   ReturnType<ReturnType<Normalizer>>;
-  // resolve(
-  //   exceptionInfo: Parameters<ReturnType<Normalizer>>[0] | undefined,
-  //   overrides?: Partial<ReturnType<ReturnType<Normalizer>> & ErrorImplements>,
-  // ): Catcher<Resolvers, ReturnType<ReturnType<Normalizer>>> &
-  //   ReturnType<ReturnType<Normalizer>>;
 }
