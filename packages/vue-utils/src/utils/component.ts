5d556dddf4b080dc1da8db3be23888e7fac5e107
dadajam4 <dadajam4@gmail.com>
2023-10-16 16:04:25 +0900

    feat(vue-utils): add typing utils

--- packages/vue-utils/src/utils/component.ts
+++ packages/vue-utils/src/utils/component.ts
@@ -3,6 +3,8 @@ import {
   VNodeChild,
   PropType,
   ComponentCustomOptions,
+  ref,
+  h,
 } from 'vue';
 import type { defineSlots } from './slots';
 
@@ -38,4 +40,151 @@ export function isComponentCustomOptions(
     (!!Component && typeof Component === 'object') ||
     typeof Component === 'function'
   );
+}
+
+type AnyComponentConstructor = { new (): any };
+
+export type ComponentInstanceType<
+  ComponentConstructor extends AnyComponentConstructor,
+> = InstanceType<ComponentConstructor>;
+
+/**
+ * API for referenceable component constructors
+ */
+export type ReferencedComponentAPI<
+  ComponentConstructor extends AnyComponentConstructor,
+> = {
+  /** component instance */
+  get instance(): ComponentInstanceType<ComponentConstructor> | undefined;
+
+  /**
+   * Ensure component instances are retrieved
+   *
+   * @throws Throws an exception if the instance is not retained
+   */
+  ensureInstance(): ComponentInstanceType<ComponentConstructor>;
+};
+
+/**
+ * Component constructor with reference
+ *
+ * @see {@link ReferencedComponentAPI}
+ */
+export type ReferencedComponent<
+  ComponentConstructor extends AnyComponentConstructor,
+> = ComponentConstructor & ReferencedComponentAPI<ComponentConstructor>;
+
+/**
+ * Creating a referenceable component constructor
+ * @param Ctor - component constructor
+ * @returns Component constructor with reference
+ *
+ * @see {@link ReferencedComponent}
+ */
+export function componentRef<
+  ComponentConstructor extends AnyComponentConstructor,
+>(Ctor: ComponentConstructor): ReferencedComponent<ComponentConstructor> {
+  const _ref = ref<ComponentInstanceType<ComponentConstructor>>();
+  const _ReferencedComponent: ReferencedComponent<ComponentConstructor> = ((
+    props: any,
+    ctx: any,
+  ) => {
+    return h(
+      Ctor,
+      {
+        ...props,
+        ref: (source: any) => {
+          _ref.value = source;
+        },
+      },
+      ctx.slots,
+    );
+  }) as any;
+
+  Object.defineProperty(_ReferencedComponent, 'instance', {
+    get: () => _ref.value,
+    enumerable: true,
+  });
+
+  _ReferencedComponent.ensureInstance = () => {
+    const instance = _ref.value;
+    if (!instance) {
+      const name = Ctor.name ? `(${Ctor.name})` : '';
+      throw new Error(
+        `The component instance${name} was not found. The ref may not be properly set within the render hook.`,
+      );
+    }
+    return instance;
+  };
+
+  return _ReferencedComponent;
+}
+
+/** Extend the component's public IF with a specified type argument */
+export type ExposedComponent<
+  Exposed extends Record<string, any>,
+  ComponentConstructor extends AnyComponentConstructor,
+> = ComponentConstructor & {
+  new (): Exposed;
+};
+
+/**
+ * Utility API to supplement type information for components
+ */
+export type TypedComponentAPI<
+  ComponentConstructor extends AnyComponentConstructor,
+> = {
+  /**
+   * Get a new constructor that extends the component's public IF type
+   *
+   * This is a support method for the inability to infer `expose()` in a jsx expression.
+   */
+  $expose<Exposed extends Record<string, any>>(): TypedComponent<
+    ExposedComponent<Exposed, ComponentConstructor>
+  >;
+  /**
+   * Creating a referenceable component constructor
+   * @returns Component constructor with reference
+   *
+   * @see {@link componentRef}
+   * @see {@link ReferencedComponent}
+   */
+  $ref(): ReferencedComponent<ComponentConstructor>;
+};
+
+/**
+ * Constructor with component type information extension API already set up
+ *
+ * @see {@link TypedComponentAPI}
+ */
+export type TypedComponent<
+  ComponentConstructor extends AnyComponentConstructor,
+> = ComponentConstructor & TypedComponentAPI<ComponentConstructor>;
+
+/**
+ * Set up a utility API for type information extension in the component constructor
+ *
+ * @param Ctor - Component constructor
+ * @returns Constructor with utility API already set up
+ */
+export function defineTypedComponent<
+  ComponentConstructor extends AnyComponentConstructor,
+>(Ctor: ComponentConstructor) {
+  const api: TypedComponentAPI<ComponentConstructor> = {
+    $expose: () => _TypedComponent,
+    $ref: () => componentRef(Ctor),
+  };
+
+  const _TypedComponent = new Proxy(
+    Ctor as TypedComponent<ComponentConstructor>,
+    {
+      get(target, propertyKey, receiver) {
+        const apiProp = (api as any)[propertyKey];
+        if (apiProp) return apiProp;
+        return Reflect.get(target, propertyKey, receiver);
+      },
+    },
+  );
+
+  return _TypedComponent;
 }
