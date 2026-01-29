0de16de383875d5f5ddbe3a68579168da51417ff
dadajam4 <dadajam4@gmail.com>
2026-01-29 13:50:59 +0900

    tmp

--- packages/vue-app-layout/src/components/VAppContainer/index.ts
+++ packages/vue-app-layout/src/components/VAppContainer/index.ts
@@ -1,4 +1,5 @@
 import * as VAppContainerStyles from './VAppContainer.css';
 
 export { VAppContainerStyles };
-export * from './VAppContainer';
+// export * from './VAppContainer';
+export { default as VAppContainer } from './VAppContainer.vue';
